3d713b3f748b59c324552482b2796362d2fb86ff
Britt Gresham <brittcgresham@gmail.com>
2016-03-02 09:06:16 +0000

    Allow multiple webroots and domains under a single cert
    Before this commit it was only possible to request a certificate for a
    single host. Now you can request certificates with multiple subdomains
    by passing in a hash to the letsencrypt_client::cert class.

--- manifests/cert.pp
+++ manifests/cert.pp
@@ -1,12 +1,12 @@
 define letsencrypt_client::cert (
-  String $webroot,
-  String $domain_name = $title,
+  Optional[String] $webroot,
+  Optional[String] $domain_name = $title,
+  Optional[Array[Hash]] $domains = undef,
+  $install_dir = $letsencrypt_client::params::install_dir,
+  $server = $letsencrypt_client::params::acme_server,
 ) {
 
   include letsencrypt_client
-
-  $install_dir = $letsencrypt_client::params::install_dir
-  $server = $letsencrypt_client::params::acme_server
 
   # This does not currently work for the certonly method of domain validation.
   # Certificates and private keys by default live in
@@ -22,8 +22,34 @@ define letsencrypt_client::cert (
   #$_options = join_keys_to_values($_formatted_hash, '=')
   #$flags = join($_options, ' ')
 
+  if ! $domains {
+    if $webroot and $domain_name {
+      $_domains = [{
+        webroot => $webroot,
+        domain_name => $domain_name,
+      }]
+    } else {
+      fail('The letsencrypt_client::cert class must be given both a webroot and domain_name.')
+    }
+  } else {
+    if $webroot or $domain_name {
+      $_domains = concat($domains, {
+        webroot => $webroot,
+        domain_name => $domain_name,
+      })
+    } else {
+      $_domains = $domains
+    }
+  }
+
+  $flags = $_domains.map |$item| {
+    "--webroot-path ${item[webroot]} -d ${item[domain_name]}"
+  }
+
+  $flags_string = join($flags, " ")
+
   exec { "${webroot}/letsencrypt/${domain_name}":
-    command => "letsencrypt certonly --webroot --webroot-path ${webroot} -d ${domain_name}",
+    command => "letsencrypt certonly --webroot ${flags_string}",
     path => "${install_dir}/bin:/usr/bin",
     unless => "openssl x509 -checkend 2592000 -noout -in /etc/letsencrypt/live/${domain_name}/cert.pem",
     require => Class['letsencrypt_client'],
